2a71f150f498f47e8ea0f96b96600bdb2b22ae0a
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 15:56:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,27 +11,27 @@ end
 
 def find_max_value(array)
   # Add your solution here
-  highest_number = 0
+  # highest_number = 0
   
-  array.length do |index|
-    if highest_number <= array[index]
-      highest_number = array[index]
-    end
-  end
-    return highest_number
+  # array.length do |index|
+  #   if highest_number <= array[index]
+  #     highest_number = array[index]
+  #   end
+  # end
+  #   return highest_number
 end
 
 
 
 def find_min_value(array)
   # Add your solution here
-    highest_number = 0
+  #   highest_number = 0
   
-  array.length do |index|
-    if highest_number <= array[index]
-      highest_number = array[index]
-    end
-  end
-    return highest_number
+  # array.length do |index|
+  #   if highest_number <= array[index]
+  #     highest_number = array[index]
+  #   end
+  # end
+  #   return highest_number
 end
 
