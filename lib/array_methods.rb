105807c86bc60c85895370ffffbaca3fe7c3f3c0
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 16:30:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,8 @@ def find_element_index(array, value_to_find)
   array.length.times do |index|
     if array[index] == value_to_find
       return index
-    else return nil
+    else 
+      return nil
     end
   end
 end
